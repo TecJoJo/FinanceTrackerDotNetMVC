6d29dd09f1b3f3041ff3ee394c359f9756b15f18
Yao Lu Home Win11 <luyao2493751@gmail.com>
2024-04-08 23:38:57 +0300

    finance management index expense list roughly done

--- FinanceTracker/Controllers/FinanceManagerController.cs
+++ FinanceTracker/Controllers/FinanceManagerController.cs
@@ -1,19 +1,21 @@
 ﻿using FinanceTracker.Models;
+using FinanceTracker.ViewModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Security.Claims;
 
 namespace FinanceTracker.Controllers
 {
-    
+
     public class FinanceManagerController : Controller
     {
         private readonly ApplicationDbContext _dbContext;
 
-        public FinanceManagerController (ApplicationDbContext dbContext)
+        public FinanceManagerController(ApplicationDbContext dbContext)
         {
-            
+
             _dbContext = dbContext;
         }
         [Authorize]
@@ -28,6 +30,7 @@ namespace FinanceTracker.Controllers
 
                 return View();
             }
+            // the user is logged into the app 
             else
             {
                 var customer = _dbContext.Customers
@@ -35,7 +38,72 @@ namespace FinanceTracker.Controllers
                                  .Include(c => c.Expenses) // Include related Orders
                                  .FirstOrDefault(e => e.CustomerId == customerId);
 
-                return View(customer);
+                //var incomes = _dbContext.Incomes
+                //                    .Where(x => x.CustomerId == customerId)
+                //                    .Select(e => new
+                //                    {
+                //                       type = "Income",
+                //                       timeStemp = e.TimeStamp,
+                //                       category = e.Category,
+                //                       amount = e.amount,
+                //                       description = e.description,
+
+
+                //                    });
+                //var expenses = _dbContext.Expenses
+                //                   .Where(x => x.CustomerId == customerId)
+                //                   .Select(e => new
+                //                   {
+                //                       type = "Expense",
+                //                       timeStemp = e.TimeStamp,
+                //                       category = e.Category,
+                //                       amount = e.amount,
+                //                       description = e.description,
+
+
+                //                   });
+
+                //var transactions = incomes.Concat(expenses);
+
+
+
+                var customerIncomes = _dbContext.Incomes
+                    .Where(i => i.CustomerId == customerId)
+                    .OrderByDescending(i => i.TimeStamp)
+                    .Take(5)
+                    .Select(i => new TransactionViewModel()
+                    {
+                        TimeStamp = i.TimeStamp,
+                        Amount = i.amount,
+                        Description = i.description,
+                        Category = i.Category.ToString(),
+                        Id = i.IncomeId,
+                        Type = "Income",
+                    })
+                    .ToList();
+
+                var customerExpenses = _dbContext.Expenses
+                    .Where(e => e.CustomerId == customerId)
+                    .OrderByDescending(e => e.TimeStamp)
+                    .Take(5)
+                    .Select(e => new TransactionViewModel()
+                    {
+                        TimeStamp = e.TimeStamp,
+                        Amount = e.amount,
+                        Description = e.description,
+                        Category = e.Category.ToString(),
+                        Id = e.ExpenseId,
+                        Type = "Expense"
+                    })
+                    .ToList();
+
+                var customerTransactions = customerIncomes
+                    .Concat(customerExpenses)
+                    .OrderByDescending(t => t.TimeStamp)
+                    .ToList();
+
+
+                return View(customerTransactions);
 
             }
         }
---
+++ FinanceTracker/ViewModel/TransactionViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FinanceTracker.ViewModel
+{
+    public class TransactionViewModel
+    {
+        public DateTime TimeStamp { get; set; }
+        public decimal Amount { get; set; }    
+        public string? Description { get; set; }
+        public string Category { get; set; }   
+        public int Id { get; set; }
+        public string Type { get; set; }   
+    }
+}

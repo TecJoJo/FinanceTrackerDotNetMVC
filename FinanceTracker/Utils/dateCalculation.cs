61e8e314aa0be411cff34c20f86de2b6a5a6185c
Yao Lu Home Win11 <luyao2493751@gmail.com>
2024-04-17 01:13:37 +0300

    barchart updated with bugs, value is not correspondent

---
+++ FinanceTracker/Utils/dateCalculation.cs
@@ -0,0 +1,15 @@
+﻿namespace FinanceTracker.Utils
+{
+    public static class dateCalculation
+    {
+        public static List<DateTime> GetLast30Days()
+        {
+            List<DateTime> dates = new List<DateTime>();
+            for (int i = 0; i < 30; i++)
+            {
+                dates.Add(DateTime.Now.AddDays(-i));
+            }
+            return dates;
+        }
+    }
+}
